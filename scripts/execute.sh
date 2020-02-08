3e317795a4bad94d86d1359e0d3294ff7bcb0209
stefanos1316 <stefanos1316@gmail.com>
2020-02-08 18:01:03 +0200

    Add build bench + sysbench + ctx clock

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # tasks=("aio-stress -s 15g -r 64k -t 3 temp" "aircrack-ng -w ../inputs/aircrack.txt ../inputs/wpa.cap" "aobench" "apache" "crafty bench quit" "tscp" \
-# 		"stockfish bench" "p7zip b" "bzip2  ../inputs/tmp_linux-4.3.tar.gz -v" "zstd ../inputs/zstd_test" "xz ../inputs/tmp_xz.txt" "byte register" \
+# 		"stockfish bench" "p7zip b" "bzip2  ../inputs/tmp_linux-5.3.tar.gz -v" "zstd ../inputs/zstd_test" "xz ../inputs/tmp_xz.txt" "byte register" \
 # 		"byte dhry2" "byte int" "byte float" "scimark2" "fhourstones" "gmpbench" "dcraw ../tasks/dcraw/DSC_50*" \
 # 		"nero2d ../inputs/nero2d.igf" "minions ../inputs/minions.minion" "hmmr -E 0.1 ../inputs/Pfam_ls ../inputs/7LES_DROME" \
 # 		"mafft --thread 1 --localpair --maxiterate 1000000 ../inputs/pyruvate_decarboxylase.fasta" \
 # 		"rodinia euler3d_cpu_double ../../inputs/missile.domn.0.2M" "rodinia lavaMD -cores $(nproc --all) -boxes1d 48" \
-# 		"rodinia sc_omp 10 30 512 65536 65536 2000 none output.txt $(nproc --all) && rm output.txt" \
+# 		"rodinia sc_omp 10 30 512 65536 65536 2000 none output.txt $(nproc --all) && rm output.txt" "openssl speed rsa4096" \
 #		"x264  -o /dev/null --slow --threads $(nproc --all) ../inputs/Bosphorus_1920x1080_120fps_420_8bit_YUV.y4m" \
-# 		"x265 ../inputs/Bosphorus_1920x1080_120fps_420_8bit_YUV.y4m  /dev/null")
+# 		"x265 ../inputs/Bosphorus_1920x1080_120fps_420_8bit_YUV.y4m  /dev/null" "build-linux-kernel" "build-gcc" "ctx_clock" \
+#		"sysbench cpu --cpu-max-prime=20000000 --threads=8 run" "sysbench memory --memory-total-size=100G --threads=8 run")
 
-tasks=("")
+tasks=("sysbench cpu --cpu-max-prime=20000000 --threads=8 run" "sysbench memory --memory-total-size=100G --threads=8 run")
 
 # Check array if more exist with the same name combine with last argument (testcase)
 function startServers {
@@ -23,8 +24,8 @@ function startServers {
 
 function getTimeInSeconds {
 	local filePath=$1
-	local minutes=`grep real ${filePath} | awk {'print $2'} | awk -F'm' '{print $1}'`
-	local seconds=`grep real ${filePath} | awk {'print $2'} | awk -F'm' '{print $2}' | awk -F',' '{print $1}'`
+	local minutes=`grep real ${filePath} | tail -1 | awk {'print $2'} | awk -F'm' '{print $1}'`
+	local seconds=`grep real ${filePath} | tail -1 | awk {'print $2'} | awk -F'm' '{print $2}' | awk -F',' '{print $1}'`
 	if [ $minutes -ne 0 ]; then
 		minutes=$((minutes * 60))
 	fi
@@ -34,11 +35,11 @@ function getTimeInSeconds {
 
 function dumpGarbage {
 	if [ -f temp ] || [ -f ao.ppm ] || [ -f game.001 ] || [ -f log.001 ] \
-	|| [ -f ../inputs/tmp_linux-4.3.tar.gz.bz2 ] || [ -f ../inputs/zstd_test ] \
+	|| [ -f ../inputs/tmp_linux-5.3.tar.gz.bz2 ] || [ -f ../inputs/zstd_test ] \
 	|| [ -f ../inputs/tmp_xz.txt.xz ] || [ -f *.tmp] || [ -f RES-multiply-* ] \
 	|| [ -f ../tasks/dcraw/*.ppm ] || [ -f bitmap0_* ]; then
 		rm temp ; rm ao.ppm ; rm game.* ; rm log.* ; rm *.tmp
-		rm ../inputs/tmp_linux-4.3.tar.gz.bz2 ; rm ../inputs/zstd_test.zst
+		rm ../inputs/tmp_linux-5.3.tar.gz.bz2 ; rm ../inputs/zstd_test.zst
 		rm ../inputs/tmp_xz.txt.xz ; rm RES-multiply-* ; rm ../tasks/dcraw/*.ppm 
 		rm bitmap0_*
 	fi
@@ -77,7 +78,7 @@ for task in "${tasks[@]}"; do
 			time (ab -n 1000000 -c 100 http://localhost:80/) 2> ../results/time_${taskName}.txt
 			getTimeInSeconds ../results/time_${taskName}.txt ;;
 		("bzip2")
-			cp ../inputs/linux-4.3.tar.gz ../inputs/tmp_linux-4.3.tar.gz
+			cp ../inputs/linux-5.3.tar.gz ../inputs/tmp_linux-5.3.tar.gz
 			time (../tasks/${benchmark}/${task}) 2> ../results/time_${taskName}.txt ;;
 		("xz")
 			cp ../inputs/xz.txt ../inputs/tmp_xz.txt
@@ -90,6 +91,20 @@ for task in "${tasks[@]}"; do
 			cd ../tasks/${benchmark}
 			time (./${task}) 2> ../../results/time_${taskName}.txt
 			cd ../../scripts ;;
+		("build-linux-kernel")
+			tar -xzvf ../inputs/linux-5.3.tar.gz
+			mv ./linux-5.3 ../tasks/${benchmark}
+			cd ../tasks/${benchmark}
+			time (./${task}) 2> ../../results/time_${taskName}.txt
+			rm -rf ./linux-5.3
+			cd ../../scripts ;;
+		("build-gcc")
+			tar -xzvf ../inputs/gcc-8.2.0.tar.gz
+			mv ./gcc-8.2.0 ../tasks/${benchmark}
+			cd ../tasks/${benchmark}
+			time (./${task}) 2> ../../results/time_${taskName}.txt
+			rm -rf ./gcc-8.2.0
+			cd ../../scripts ;;	
 		rodinia*)
 			cd ../tasks/${benchmark}
 			time (./${task}) 2> ../../results/time_${taskName}.txt
