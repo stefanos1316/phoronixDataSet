6c21c07e2c95e4b396a89eedca61a7b77536ad71
stefanos1316 <stefanos1316@gmail.com>
2020-02-13 17:49:15 +0200

    Add two more

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -43,7 +43,7 @@ stasks=("aio-stress -s 15g -r 64k -t 3 temp" "aircrack-ng -w ../inputs/aircrack.
 		"ramspeed copy_int" "ramspeed scale_int" "ramspeed add_int" "ramspeed triad_int" "ramspeed copy_float" "ramspeed scale_float" \
 		"ramspeed add_float" "ramspeed traid_float" "botan AES-256" "botan Blowfish" "botan CAST-256" "botan KASUMI" "botan Twofish" "gnupg") 
 # timeConsumingTaks=("povray -benchmark <<< 1" "build-linux-kernel" "build-gcc" )
-tasks=("aio-stress -s 15g -r 64k -t 3 temp" "aircrack-ng")
+tasks=("apache")
 
 # Check array if more exist with the same name combine with last argument (testcase)
 function startServers {
--- tools/downloadAndInstallTasks.sh
+++ tools/downloadAndInstallTasks.sh
@@ -3,40 +3,53 @@
 taskDirectory='tasks_test'
 taskScripts='scripts'
 mkdir tasks_test 
+cd tasks_test
 
-echo "-------Downloading and installing aio-stress"
-mkdir aio-stress
-cd aio-stress
-wget http://fsbench.filesystems.org/bench/aio-stress.c
-ls
-cc -Wall -pthread -o aio-stress aio-stress.c -laio
-cd ../
-mv aio-stress ${taskDirectory}/
+# echo "-------Downloading and installing aio-stress"
+# mkdir aio-stress
+# cd aio-stress
+# wget http://fsbench.filesystems.org/bench/aio-stress.c
+# cc -Wall -pthread -o aio-stress aio-stress.c -laio
+# cd ../
 
-echo "-------Downloading and installing aircrack-ng"
-mkdir aircrack-ng
-cd aircrack-ng
-wget https://download.aircrack-ng.org/aircrack-ng-1.3.tar.gz
-tar -zxvf aircrack-ng-1.3.tar.gz && rm aircrack-ng-1.3.tar.gz
-mv aircrack-ng-1.3/* ./
-rm -rf aircrack-ng-1.3/
-./autogen.sh
-make -j $(nproc --all)
-cp ../${taskScripts}/aircrack-ng ./
-cd ../
-mv aircrack-ng ${taskDirectory}/
+# echo "-------Downloading and installing aircrack-ng"
+# mkdir aircrack-ng
+# cd aircrack-ng
+# wget https://download.aircrack-ng.org/aircrack-ng-1.3.tar.gz
+# tar -zxvf aircrack-ng-1.3.tar.gz && rm aircrack-ng-1.3.tar.gz
+# mv aircrack-ng-1.3/* ./
+# rm -rf aircrack-ng-1.3/
+# ./autogen.sh
+# make -j $(nproc --all)
+# cp ../${taskScripts}/aircrack-ng ./
+# cd ../
 
-echo "-------Downloading and install gnupg"
-mkdir gnupg && cd gnupg
-wget http://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.19.tar.bz2
-tar -jxvf gnupg-2.2.19.tar.bz2 && rm gnupg-2.2.19.tar.bz2
-mv gnupg-2.2.19/* ./ && rm -rf gnupg-2.2.19 
-cp ${taskScripts}/gnupg ./
-./configure --prefix=`pwd`
-make -j $(nproc --all)
-make install
-echo pts-1234567890 > passphrase
-cd ../ && mv gnupg ${taskDirectory}/
+# echo "-------Downloading and installing aobench"
+# mkdir aobench && cd aobench
+# wget http://phoronix-test-suite.com/benchmark-files/aobench-20180207.zip
+# unzip aobench-20180207.zip && rm aobench-20180207.zip
+# cc ao.c -o aobench -lm -O3
+
+echo "-------Downloading and installing apache"
+mkdir apache && cd apache
+wget http://archive.apache.org/dist/httpd/httpd-2.4.29.tar.bz2
+tar -xjvf httpd-2.4.29.tar.bz2 && rm httpd-2.4.29.tar.bz2
+mv httpd-2.4.29/* ./ && rm -rf httpd-2.4.29/
+./configure
+make --j $(nproc --all)
+sudo make install
+
+# echo "-------Downloading and install gnupg"
+# mkdir gnupg && cd gnupg
+# wget http://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.19.tar.bz2
+# tar -jxvf gnupg-2.2.19.tar.bz2 && rm gnupg-2.2.19.tar.bz2
+# mv gnupg-2.2.19/* ./ && rm -rf gnupg-2.2.19 
+# cp ${taskScripts}/gnupg ./
+# ./configure --prefix=`pwd`
+# make -j $(nproc --all)
+# make install
+# echo pts-1234567890 > passphrase
+# cd ../ && mv gnupg ${taskDirectory}/
 
 # echo "-------Downloading and installing botan"
 # mkdir -p ../${taskDirectory}/botan
