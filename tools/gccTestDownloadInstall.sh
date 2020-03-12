53f9d2875c9f3ceef22b3f87e529a26afd39980a
stefanos1316 <stefanos1316@gmail.com>
2020-03-12 17:15:49 +0200

    Add more test

--- tools/gccTestDownloadInstall.sh
+++ tools/gccTestDownloadInstall.sh
@@ -15,6 +15,139 @@ taskScripts='scripts'
 mkdir gcc_tasks_test
 cd gcc_tasks_test
 
+echo "-------Downloading and installing xz"
+mkdir xz && cd xz
+wget http://distfiles.macports.org/xz/xz-5.2.4.tar.bz2
+tar -xvf xz-5.2.4.tar.bz2 && rm xz-5.2.4.tar.bz2
+mv  xz-5.2.4/* ./ && rm -rf xz-5.2.4/
+CFLAGS="${SECURITY_FLAGS}" ./configure
+make -j $(nproc --all)
+echo "#!/bin/bas
+cp ../../../inputs/xz.txt tmp_xz.txt
+if [ -f \"tmp_xz.txt.xz\" ]; then
+	rm -f tmp_xz.txt.xz
+fi
+xz tmp_xz.txt" > xz
+chmod +x xz
+cd ../
+
+echo "-------Downloading and installing BYTE"
+mkdir byte && cd byte
+wget http://www.phoronix-test-suite.com/benchmark-files/byte-benchmark-2.tar.gz
+tar -zxvf byte-benchmark-2.tar.gz && rm byte-benchmark-2.tar.gz
+mv  bm/* ./ && rm -rf bm/
+make clean
+if [ ! -z "$SECURITY_FLAGS" ]; then
+    toReplace=`echo $SECURITY_FLAGS | sed  's/\ /\\\ /g'`
+    sed -i 's/-DTIME/-DTIME\ '"$toReplace"'/g' Makefile
+fi
+make
+mv Run byte
+cd ../
+
+exit
+
+echo "-------Downloading and installing p7zip"
+mkdir p7zip && cd p7zip
+wget http://ftp.osuosl.org/pub/blfs/conglomeration/p7zip/p7zip_16.02_src_all.tar.bz2
+tar -xjvf p7zip_16.02_src_all.tar.bz2 && rm p7zip_16.02_src_all.tar.bz2
+mv p7zip_16.02/* ./ && rm -rf p7zip_16.02
+make -j $(nproc --all)
+cp bin/7za ./p7zip
+cd ../
+
+echo "-------Downloading and installing zstd"
+mkdir zstd && cd zstd
+wget http://www.phoronix-test-suite.com/benchmark-files/zstd-1.3.4.tar.gz
+tar -xvf zstd-1.3.4.tar.gz && rm zstd-1.3.4.tar.gz
+mv  zstd-1.3.4/* ./ && rm -rf zstd-1.3.4/
+if [ ! -z "$SECURITY_FLAGS" ]; then
+    toReplace=`echo $SECURITY_FLAGS | sed  's/\ /\\\ /g'`
+    sed -i 's/-O3/-O3\ '"$toReplace"'/g' Makefile
+fi
+make -j $(nproc --all)
+mv zstd zstd-bin
+cp ../../../inputs/linux-5.3.tar.gz ./
+echo "#!/bin/bash
+./zstd-bin -19 -T\$(nproc --all) linux-5.3.tar.gz
+rm -f linux-5.3.tar.gz.zst" > zstd
+chmod +x zstd
+cd ../
+
+echo "-------Downloading and installing tscp"
+mkdir tscp && cd tscp
+wget http://www.phoronix-test-suite.com/benchmark-files/tscp181_pts.tar.bz2
+tar -xjvf tscp181_pts.tar.bz2 && rm tscp181_pts.tar.bz2
+mv tscp181/* ./ && rm -rf tscp181
+cp ../../${taskScripts}/tscp_main.c ./main.c
+cc -O3 ${SECURITY_FLAGS} *.c -o tscp
+cd ../
+
+echo "-------Downloading and installing stockfish"
+mkdir stockfish && cd stockfish
+wget http://www.phoronix-test-suite.com/benchmark-files/stockfish-9-src.zip
+unzip stockfish-9-src.zip && rm stockfish-9-src.zip
+cd src
+if [ ! -z "$SECURITY_FLAGS" ]; then
+    toReplace=`echo $SECURITY_FLAGS | sed  's/\ /\\\ /g'`
+    sed -i 's/-Wall/-Wall\ '"$toReplace"'/g' Makefile
+fi
+make build ARCH=x86-64-modern
+cp stockfish ../stockfish-bin
+cd ../
+echo "#!/bin/bash
+for i in {1..20}; do
+	./stockfish-bin bench
+done" > stockfish
+chmod +x stockfish
+cd ../
+
+echo "-------Downloading and installing crafty"
+mkdir crafty && cd crafty
+wget http://craftychess.com/downloads/source/crafty-25.2.zip
+unzip crafty-25.2.zip && rm crafty-25.2.zip
+if [ ! -z "$SECURITY_FLAGS" ]; then
+    toReplace=`echo $SECURITY_FLAGS | sed  's/\ /\\\ /g'`
+    sed -i 's/-O3/-O3\ '"$toReplace"'/g' Makefile
+fi
+make unix-gcc
+cd ../
+
+echo "-------Downloading and installing aobench"
+mkdir aobench && cd aobench
+wget http://phoronix-test-suite.com/benchmark-files/aobench-20180207.zip
+unzip aobench-20180207.zip && rm aobench-20180207.zip
+cc ${SECURITY_FLAGS} ao.c -o aobench -lm -O3
+cd ../
+
+echo "-------Downloading and installing ctx-clock"
+mkdir ctx_clock && cd ctx_clock
+cp ../../$taskScripts/ctx_clock.c ./ctx-clock.c
+cc ${SECURITY_FLAGS} ctx-clock.c -o ctx-clock 
+echo "#!/bin/bash
+for i in {1..10}; do
+    ./ctx-clock
+done" > ctx_clock
+chmod +x ctx_clock
+cd ../
+
+echo "-------Downloading and installing aircrack-ng"
+mkdir aircrack-ng && cd aircrack-ng
+wget https://download.aircrack-ng.org/aircrack-ng-1.3.tar.gz
+tar -zxvf aircrack-ng-1.3.tar.gz && rm aircrack-ng-1.3.tar.gz
+mv aircrack-ng-1.3/* ./
+rm -rf aircrack-ng-1.3/
+./autogen.sh
+if [ ! -z "$SECURITY_FLAGS" ]; then
+    toReplace=`echo $SECURITY_FLAGS | sed  's/\ /\\\ /g'`
+    sed -i 's/-fstack-protector-strong/'"$toReplace"'/g' Makefile
+fi
+make -j $(nproc --all)
+echo "#!/bin/bash
+./src/aircrack-ng -w ../../../inputs/aircrack.txt ../../../inputs/wpa.cap" > aircrack-ng
+chmod +x aircrack-ng
+cd ../
+
 echo "-------Downloading and installing bzip2"
 mkdir bzip2 && cd bzip2
 wget http://downloads.openwrt.org/sources/bzip2-1.0.6.tar.gz
@@ -47,8 +180,6 @@ cd bin/
 mpirun --allow-run-as-root -np 8 ./xhpcg" > hpcg
 chmod +x hpcg
 cd ../
-
-exit
 
 echo "-------Downloading and installing povray"
 mkdir povray && cd povray
