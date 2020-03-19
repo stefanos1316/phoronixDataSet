e90838b75c598b4c9c49691a4e36994df9e7fea1
stefanos1316 <stefanos1316@gmail.com>
2020-03-20 00:21:07 +0200

    Add draft of results

--- results/mergeResults.sh
+++ results/mergeResults.sh
@@ -2,4 +2,17 @@
 
 dataType=$1
 
-paste <(cat stock_perf/${dataType}.txt) <(awk '{print $2}' meltdown_perf/${dataType}.txt) <(awk '{print $2}' spectre_perf/${dataType}.txt) <(awk '{print $2}' mds_perf/${dataType}.txt) > ${dataType}.txt
+echo Tasks	Stock	Meltdown	Spectre		MDS > ${dataType}.txt
+paste <(awk '{print $2}' stock_perf/${dataType}.txt) <(awk '{print $2}' meltdown_perf/${dataType}.txt) <(awk '{print $2}' spectre_perf/${dataType}.txt) <(awk '{print $2}' mds_perf/${dataType}.txt) >> ${dataType}_beforelog10.txt
+
+while read -r line; do
+	logLine=''
+	for i in ${line[@]}; do
+		logLine+=`echo "scale=2 ; l($i)/l(1.5)" | bc -l`
+		logLine+="  "
+	done
+	echo $logLine >> tmp.txt
+done <${dataType}_beforelog10.txt
+
+paste <(awk '{print $1}' stock_perf/${dataType}.txt) <(cat ${dataType}_beforelog10.txt) >> ${dataType}.txt
+rm tmp.txt; rm ${dataType}_beforelog10.txt
